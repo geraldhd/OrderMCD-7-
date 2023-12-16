5042c00708d1ded86e9f3144cbd5f089d4130b4b
geraldhd <125950426+geraldhd@users.noreply.github.com>
2023-12-16 23:00:22 +0800

    dah

--- src/mcd/App.java
+++ src/mcd/App.java
@@ -2,11 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package mcd;
 
 /**
  *
- * @author Lenovo
+ * @author ASUS
  */
-class txt_nama {
-    
+public class App {
+    public static void main(String[] args) {
+	new Login();
+    }
 }
---
+++ src/mcd/DatabaseManager.java
@@ -0,0 +1,117 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    // JDBC URL, username, and password of MySQL server
+    private static final String URL = "jdbc:mysql://localhost:3306/order_mcd";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    // JDBC variables for opening, closing, and managing connection
+    private static Connection connection;
+
+    // Method to configure and return a database connection
+    private static Connection configDB() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    // Method to close the database connection
+    private static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    // Metode untuk menyimpan pesanan
+    public static void savePesanan(int idPesanan, int totalHarga) {
+        try {
+            Connection conn = configDB();
+            String sql = "INSERT INTO pesanan (id_pesanan, total_harga) VALUES (?, ?)";
+            try (PreparedStatement pst = conn.prepareStatement(sql)) {
+                pst.setInt(1, idPesanan);
+                pst.setInt(2, totalHarga);
+                pst.executeUpdate();
+            }
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Metode untuk mendapatkan semua pesanan
+    public static void getAllPesanan() {
+        try {
+            Connection conn = configDB();
+            String sql = "SELECT * FROM pesanan";
+            try (PreparedStatement pst = conn.prepareStatement(sql);
+                 ResultSet rs = pst.executeQuery()) {
+
+                while (rs.next()) {
+                    int idPesanan = rs.getInt("id_pesanan");
+                    int totalHarga = rs.getInt("total_harga");
+                    System.out.println("ID Pesanan: " + idPesanan + ", Total Harga: " + totalHarga);
+                }
+            }
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Metode untuk menyimpan data user
+    public static void saveUser(int id, String nama) {
+        try {
+            Connection conn = configDB();
+            String sql = "INSERT INTO user (id, nama) VALUES (?, ?)";
+            try (PreparedStatement pst = conn.prepareStatement(sql)) {
+                pst.setInt(1, id);
+                pst.setString(2, nama);
+                pst.executeUpdate();
+            }
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Metode untuk mendapatkan semua user
+    public static void getAllUser() {
+        try {
+            Connection conn = configDB();
+            String sql = "SELECT * FROM user";
+            try (PreparedStatement pst = conn.prepareStatement(sql);
+                 ResultSet rs = pst.executeQuery()) {
+
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nama = rs.getString("nama");
+                    System.out.println("ID User: " + id + ", Nama: " + nama);
+                }
+            }
+            closeConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example of how to use the DatabaseManager class
+        savePesanan(1, 100);
+        getAllPesanan();
+
+        saveUser(1, "John Doe");
+        getAllUser();
+    }
+}
